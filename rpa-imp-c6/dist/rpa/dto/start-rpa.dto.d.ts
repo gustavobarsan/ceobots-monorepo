c92b919a301057f22663ce2f9be28b08ff72936b
gustavobarsan <gustavobarsan@gmail.com>
2026-06-10 16:51:47 -0300

    chore: remove tracked dist build files from C6 RPA workspace

--- rpa-imp-c6/dist/rpa/dto/start-rpa.dto.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class StartRpaDto {
-    usuario: string;
-    senha: string;
-    loja: string;
-    headless?: boolean;
-    statusImportacao?: string[];
-    callbackUrl: string;
-}
--- rpa-imp-c6/dist/rpa/rpa.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { StartRpaDto } from './dto/start-rpa.dto';
-export declare class RpaService {
-    private readonly logger;
-    private activeBrowsers;
-    startExtraction(dto: StartRpaDto): Promise<void>;
-    stopAll(): void;
-}
